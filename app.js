9e2447ec2c83d0d6d625c9cb3c3e7b704029fe14
iamhanori <s2122@e-mirim.hs.kr>
2023-05-11 19:43:40 +0900

    insert 수정 필요 / edit 추가하기

--- app.js
+++ app.js
@@ -2,45 +2,47 @@ let express = require('express');
 let ejs = require('ejs');
 let path = require('path');
 
-let app = express();
 let todoArr = [
-    { id : 1, contents : 'movie', yesno : 'no'},
-    { id : 2, contents : 'homework', yesno : 'no'},
-    { id : 3, contents : 'sleep', yesno : 'no'}
-]; // 빈 배열
+    {id : 1, contents : '영화보기', yesno : 'no'},
+    {id : 2, contents : '집에가기', yesno : 'no'},
+    {id : 3, contents : '잠자기', yesno : 'no'}
+]; //빈 배열
 
+let app = express();
 
-
-app.use(express.static('public')); // public 폴더 공유
-app.use(express.urlencoded({extended:false})); // 사용자
-app.set('views', path.join(__dirname, 'views')); // views
+app.use(express.static('public')); //public 폴더 공유
+app.use(express.urlencoded( {extended:false})); // 사용자
+app.set('views', path.join(__dirname, 'views')); // view 폴더
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
-    console.log('/get이 시작');
-    res.render('list', {datalist : todoArr}); // list.ejs
-})
+    console.log('/ get 시작');
+    res.render('list',  {datalist: todoArr}); //list.ejs
+});
 
 app.get('/insert', (req, res) => {
-    console.log('/insert 이 시작');
+    console.log("/insert get 시작");
+    // 추가
+    res.render('insert');
+})
+
+app.post('/insert', (req, res) => {
+    console.log("/insert post 시작");
     todoArr.push({contents: req.body.contents, yesno: req.body.yesno});
 })
 
-app.get('/post', (req, res) => {
-    console.log('/post 이 시작');
-    
-})
-
-
 app.get('/delete/:id', (req, res) => {
-    console.log('/delete id' + res.id);
-    console.log('/delete id' + res.id);
+    console.log("/delete " + res.id);
+    // 추가
     todoArr.splice(res.id,1);
-    console.log('deleted.' + res.id);
+    console.log("/delete ok" + res.id);
     res.redirect("/");
 })
 
+app.get('/edit/:id', (req, res) => {
+    
+})
+
 app.listen(3000, () => {
-    console.log("3000 port");
+    console.log(`3000포트 서버 시작`);
 })
