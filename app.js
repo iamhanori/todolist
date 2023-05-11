6cf11e566ad9e04a8551f042c12167c3160aa45a
iamhanori <s2122@e-mirim.hs.kr>
2023-05-11 19:15:42 +0900

    insert, delete 기능

--- app.js
+++ app.js
@@ -24,19 +24,21 @@ app.get('/', (req, res) => {
 
 app.get('/insert', (req, res) => {
     console.log('/insert 이 시작');
-    // 추가
-    
+    todoArr.push({contents: req.body.contents, yesno: req.body.yesno});
 })
 
 app.get('/post', (req, res) => {
     console.log('/post 이 시작');
-    // 추가
+    
 })
 
 
 app.get('/delete/:id', (req, res) => {
     console.log('/delete id' + res.id);
-    
+    console.log('/delete id' + res.id);
+    todoArr.splice(res.id,1);
+    console.log('deleted.' + res.id);
+    res.redirect("/");
 })
 
 app.listen(3000, () => {
