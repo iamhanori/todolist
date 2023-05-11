fdf8ea22538d0d3d41345f54adaf96baac2c9ea0
iamhanori <s2122@e-mirim.hs.kr>
2023-05-11 19:14:52 +0900

    TODO LIST 틀

---
+++ app.js
@@ -0,0 +1,44 @@
+let express = require('express');
+let ejs = require('ejs');
+let path = require('path');
+
+let app = express();
+let todoArr = [
+    { id : 1, contents : 'movie', yesno : 'no'},
+    { id : 2, contents : 'homework', yesno : 'no'},
+    { id : 3, contents : 'sleep', yesno : 'no'}
+]; // 빈 배열
+
+
+
+app.use(express.static('public')); // public 폴더 공유
+app.use(express.urlencoded({extended:false})); // 사용자
+app.set('views', path.join(__dirname, 'views')); // views
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    console.log('/get이 시작');
+    res.render('list', {datalist : todoArr}); // list.ejs
+})
+
+app.get('/insert', (req, res) => {
+    console.log('/insert 이 시작');
+    // 추가
+    
+})
+
+app.get('/post', (req, res) => {
+    console.log('/post 이 시작');
+    // 추가
+})
+
+
+app.get('/delete/:id', (req, res) => {
+    console.log('/delete id' + res.id);
+    
+})
+
+app.listen(3000, () => {
+    console.log("3000 port");
+})
